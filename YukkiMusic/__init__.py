e416afaf4b215e0775e7540a377238b2c2a4b7a0
Amrit Raj <89985450+amritrajjj@users.noreply.github.com>
2022-05-31 21:03:06 +0530

    Update __init__.py

--- YukkiMusic/__init__.py
+++ YukkiMusic/__init__.py
@@ -11,7 +11,7 @@ from YukkiMusic.core.bot import YukkiBot
 from YukkiMusic.core.dir import dirr
 from YukkiMusic.core.git import git
 from YukkiMusic.core.userbot import Userbot
-from YukkiMusic.misc import dbb, heroku, sudo
+from YukkiMusic.misc import dbb, sudo
 
 from YukkiMusic.logging import LOGGER
 
@@ -23,9 +23,6 @@ git()
 
 # Initialize Memory DB
 dbb()
-
-# Heroku APP
-heroku()
 
 # Load Sudo Users from DB
 sudo()
